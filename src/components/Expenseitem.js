2a9f611f4ba525b065299444289891c522f1a6dc
satishhalhalli <satishhalhalli7484@gmail.com>
2023-03-11 11:41:47 +0530

    first

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,9 @@
+function ExpenseItem(){
+    return (<><h2>Expense Item!</h2>
+    <h2>Food Rs 10 </h2>
+    <h2>petrol rs 100</h2>
+    <h2>movies Rs 1000</h2>
+    </>)
+}
+
+export default ExpenseItem;
